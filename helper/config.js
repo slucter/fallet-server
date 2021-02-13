3cb977644fc8c9c5923b01984a195337a473012a
slucter <slucter133@gmail.com>
2021-02-13 20:28:11 +0700

    use host smtp.gmail

--- helper/config.js
+++ helper/config.js
@@ -1,7 +1,9 @@
 require('dotenv')
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.MAIL,
         pass: process.env.MAIL_PASS,
